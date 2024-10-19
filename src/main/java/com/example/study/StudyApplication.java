34d68558b71caa20a10358023fc99a3a5c50d573
heeyoungha <aotolove@naver.com>
2024-10-19 16:23:57 +0900

    db연결 디버깅

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -2,12 +2,24 @@ package com.example.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class StudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(StudyApplication.class, args);
+        DataSource dataSource = context.getBean(DataSource.class);
+        try (Connection conn = dataSource.getConnection()) {
+            System.out.println("Connected to database: " + conn.getMetaData().getURL());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
