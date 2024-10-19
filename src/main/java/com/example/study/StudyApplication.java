8d65fe1c29a66ce426040c4ddd6bab1cc3e44476
heeyoungha <aotolove@naver.com>
2024-10-19 18:45:03 +0900

    entityScan 설정

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,7 +13,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @SpringBootApplication
-@EnableTransactionManagement
+@EntityScan(basePackages = "com.example")
 public class StudyApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(StudyApplication.class);
