25bd81b7c81316140ac6fb50e568beb23e08a981
heeyoungha <aotolove@naver.com>
2024-10-26 16:42:22 +0900

    StudyApplication.java @EnableJpaAuditing 삭제

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -13,6 +14,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EntityScan(basePackages = "com.example")
 public class StudyApplication {
 
