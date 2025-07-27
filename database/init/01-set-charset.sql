1250d6fcd30172166b80c69c99c784ba5e0b21d9
heeyoungha <aotolove@naver.com>
2025-07-27 22:05:55 +0900

    [fix] db utf8로 수정

---
+++ database/init/01-set-charset.sql
@@ -0,0 +1,9 @@
+-- 문자셋 설정
+SET NAMES utf8mb4;
+SET CHARACTER SET utf8mb4;
+SET character_set_client = utf8mb4;
+SET character_set_connection = utf8mb4;
+SET character_set_results = utf8mb4;
+
+-- 데이터베이스 문자셋 설정
+ALTER DATABASE study CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
